9e6a9676eb03a0a83056ddcaed7086745738de86
NGASSAI <comptengassai@gmail.com>
2026-09-01 01:45:56 +0100

    mis à jours du portfolio

--- data/portfolio.ts
+++ data/portfolio.ts
@@ -78,7 +78,7 @@ export const projects: Project[] = [
   {
     title:'codeDoctor',
     description:
-    "Une appli d'accompagnement pour les dévéloppeurs , permettant de les aidez à résoudre les bugs pendant le travail et aide aussi à cultiver ce désire de comprendre les bug",
+    "Une appli d'accompagnement pour les dévéloppeurs , permettant de les aidez à résoudre les bugs pendant le travail et aide aussi à cultiver se désire de comprendre les bug",
     image:'/codeDoctor.png',
     technologies:['NextJs','postgreSQL','NodeJs','tailwindcss'],
     github:'https://github.com/NGASSAI/codeDoctor-front',
